20db8f3ad538c8f666dc7aa40be7f4092eca3019
AnonymouslyFast <calebshearer1@gmail.com>
2024-10-27 02:07:58 -0400

    V0.1: basic window creation

---
+++ src/Application.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include "../Dependencies/include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+// Prototypes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// Magic happens here
+int main() {
+
+    // Checks if glfw is properly initialized
+    if (!glfwInit()) {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Changing a few glfw settings
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Creating the window
+    GLFWwindow* window = glfwCreateWindow(600, 800, "MLDC", NULL, NULL);
+      // Checks if something went wrong creating the window and terminates glfw
+    if (window == NULL) {
+        std::cout << "Failed to open GLFW window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+      // opens the window
+    glfwMakeContextCurrent(window);
+
+    // Checks if glad has a problem
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 600, 800); // setting the viewport for lower left corner
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // While loop to keep the window up
+    while (!glfwWindowShouldClose(window)) {
+
+        // Processing inputs ya know
+        processInput(window);
+
+        // Render shit go here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // other non-important stuff to comment
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate(); // Cleaning the trash
+    return 0;
+}
+
+// When user resizes window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height); // resizing the viewport for the new window size to the lower left corner
+}
+
+// Put ya inputs in here ya dork
+void processInput(GLFWwindow* window) {
+
+    // when user presses ESC the window will close
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+
+}
