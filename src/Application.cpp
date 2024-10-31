2bea336659b04740d8fe475a525294c74f1da5c0
AnonymouslyFast <calebshearer1@gmail.com>
2024-10-30 21:41:20 -0400

    V0.5: Added textures and transformations
    Took 2 hours 33 minutes

--- src/Application.cpp
+++ src/Application.cpp
@@ -4,6 +4,11 @@
 #include "Shader.h"
 
 #include <iostream>
+#include "../Dependencies/include/stb_image/stb_image.h"
+#include "../Dependencies/include/glm/glm.hpp"
+#include "../Dependencies/include/glm/gtc/matrix_transform.hpp"
+#include "../Dependencies/include/glm/gtc/type_ptr.hpp"
+
 
 
 // Prototypes
@@ -52,20 +57,20 @@ int main() {
     // Creating Shaders (Yes ik I am using full path cuz I didn't wanna spend YEARS on this shader shit)
     Shader shader(R"(C:\Users\caleb\OneDrive\Desktop\c++\opengl_test\assets\shaders\main_vertex.glsl)", R"(C:\Users\caleb\OneDrive\Desktop\c++\opengl_test\assets\shaders\main_fragment.glsl)");
 
-
     // Creating a rectangle
 
-    float vertices[] = {    // Colors
-        0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
-        -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f  // top left
-    };
+    float vertices[] = {
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+   };
 
 
     unsigned int indices[] = {
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
 
@@ -86,14 +91,38 @@ int main() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
         // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
         // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+        // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);  // can safely unbind
 
+    // Creating a texture
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(R"(C:\Users\caleb\OneDrive\Desktop\c++\opengl_test\assets\textures\container.jpg)", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+
+    stbi_image_free(data);
 
 
     // While loop to keep the window up
@@ -105,6 +134,23 @@ int main() {
         // Render shit go here
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+            // Binding texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        float timeValue = glfwGetTime();
+
+        // Creating transformations
+        glm::mat4 transform = glm::mat4(1.0f); // init to a identity matrix
+        transform = glm::rotate(transform, timeValue, glm::vec3(0.0f, 0.0f, 1.0f));
+            // Setting the transform matrix
+        unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        // Changing colors
+        float greenValue = sin(timeValue*1.5) / 6.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shader.ID, "color");
+        glUniform3f(vertexColorLocation, 1.0f, greenValue, 0.0f);
 
             // Rendering Rectangle
         shader.use();
